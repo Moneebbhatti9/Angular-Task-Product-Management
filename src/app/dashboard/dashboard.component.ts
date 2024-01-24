050e33566a72b19cc20ffd3a00dab3d1a1247309
Moneeb <moneebbhatti987@gmail.com>
2024-01-24 16:32:43 +0500

    modified:   src/app/dashboard/dashboard.component.css 	modified:   src/app/dashboard/dashboard.component.html 	modified:   src/app/dashboard/dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MainService } from '../services/main.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  constructor(private mainService: MainService) {}
+  totalCategoriesCount: number = this.mainService.getCategories().length;
+  totalProductsCount: number = this.mainService.getProducts().length;
+  totalVendorsCount: number = this.mainService.getVendors().length;
 }
